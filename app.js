cd3f6e11790cb46620e0d75455b051ef05a02311
Parth Dhawan <pdhawan2001@gmail.com>
2021-09-14 02:53:08 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const express = require('express');
+
+const app = express();
+
+app.use(express.json()); //middleware
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  console.log(req.body);
+  res.send('Done');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
