b1438a8550081e083019dac463ced71911de5fa4
Parth Dhawan <pdhawan2001@gmail.com>
2021-09-25 02:43:28 +0530

    connected db with express

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
+
 
 dotenv.config({path: './config.env'});
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>', 
+  process.env.DATABASE_PASSWORD
+);
 
-// console.log(process.env); // tells the environment we are in
- 
+mongoose.connect(DB)
+.then(() => console.log('DB connection successful')); // to connect to mongodb server, second to deal with some deperecation warnings
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
-});
+}); 
