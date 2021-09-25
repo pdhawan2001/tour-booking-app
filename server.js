a07f0509aa5415b504c08119cbd501a0654578f9
Parth Dhawan <pdhawan2001@gmail.com>
2021-09-25 16:49:39 +0530

    created Tour model

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
-
 dotenv.config({path: './config.env'});
 
 const DB = process.env.DATABASE.replace(
@@ -13,7 +12,25 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB)
 .then(() => console.log('DB connection successful')); // to connect to mongodb server, second to deal with some deperecation warnings
 
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true, // if false, error is specified
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+}); // this is schema
+
+const Tour = mongoose.model('Tour', tourSchema); // this is model
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
-}); 
+});
