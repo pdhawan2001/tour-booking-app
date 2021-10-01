8414b9b6eb9b7d52e77a1f82bbe3634e2c809777
Parth Dhawan <pdhawan2001@gmail.com>
2021-10-01 17:06:23 +0530

    refactored API features

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,69 +1,89 @@
 const Tour = require('../models/tourModel');
 
 exports.aliasTopTours = (req, res, next) => {
-  req.query.limit= '5';
+  req.query.limit = '5';
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name, price, ratingsAverage, summary, difficulty';
   next();
 };
 
+class APIFeatures {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  } // constructor automatically gets called when we create an new object out of a class
+
+  filter() {
+    // 1A) FILTERING
+    const queryObj = { ...this.queryString }; // copy of req.query obj
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    // 1B) ADVANCE FILTERING
+    let queryString = JSON.stringify(queryObj);
+    queryString = queryString.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    ); // replacing with $match, first dollar for syntax, second for replacing, \b is added to replace multiple times
+    console.log(JSON.parse(queryString));
+
+    this.query = this.query.find(JSON.parse(queryString));
+    return this; // this is the entire object
+  }
+
+  sort() {
+    if (this.queryString.sort) {
+      const sortBy = this.queryString.sort.split(',').join(' ');
+      console.log(sortBy);
+      this.query = this.query.sort(sortBy);
+      // sort('price ratingAverage'); // if two prices are same, they'll be sorted based on
+    } else {
+      this.query = this.query.sort('-createdAt');
+    }
+    return this;
+  }
+
+  limitFields() {
+    if (this.queryString.fields) {
+      const fields = this.queryString.fields.split(',').join(' ');
+      this.query = this.query.select(fields); // this method of selecting certain field names is called projecting
+    } else {
+      this.query = this.query.select('-__v'); // it will exclude this field
+    }
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1; // * 1 to convert a string to a number // by default page 1
+    const limit = this.queryString.limit * 1 || 100;
+    const skip = (page - 1) * limit; // formula derived from below comments
+
+    // page=2&limit=10 // 1-10 are page 1, 11-20 are page 2, 21-30 are page 3 and so on
+    this.query = this.query.skip(skip).limit(limit);
+
+    // if (req.query.page) {
+    //   const numTours = await Tour.countDocuments(); // method which will count the documents on page, like number of tours
+    //   if (skip >= numTours) throw new Error('This page does not exit!!');
+    // }   // error if no results commented for practice
+
+    return this;
+  }
+}
+
 exports.getAllTours = async (req, res) => {
   try {
     console.log(req.query);
 
-    // BUILD QUERY
-    // 1A) FILTERING
-    const queryObj = {...req.query};  // copy of req.query obj
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach(el => delete queryObj[el]);
-
-    // 1B) ADVANCE FILTERING
-    let queryString = JSON.stringify(queryObj);
-    queryString = queryString.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryString));
-
-    let query =  Tour.find(JSON.parse(queryString));
-    
-    // { difficulty: 'easy', duration: { $gte: 5 }} // mongoDB practice object
-    // { difficulty: 'easy', duration: { gte: '5' } } // query log
-    // gte, gt, lte, lt
-        
-    // 2) SORTING 
-    if(req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
-      query = query.sort(sortBy);
-      // sort('price ratingAverage'); // if two prices are same, they'll be sorted based on 
-    } else {
-      query = query.sort('-createdAt');
-    }
-
-    // 3) FIELD LIMITING 
-    if (req.query.fields) {
-      const fields = req.query.fields.split(',').join(' ');
-      query = query.select(fields); // this method of selecting certain field names is called projecting
-    } else {
-      query = query.select('-__v'); // it will exclude this field
-    }
-
-    // 4) PAGINATION
-    const page = req.query.page * 1 || 1 ;  // * 1 to convert a string to a number // by default page 1
-    const limit = req.query.limit * 1 || 100;
-    const skip =  (page - 1) * limit; // formula derived from below comments
-
-    // page=2&limit=10 // 1-10 are page 1, 11-20 are page 2, 21-30 are page 3 and so on
-    query = query.skip(skip).limit(limit);
-
-    if(req.query.page) {
-      const numTours = await Tour.countDocuments(); // method which will count the documents on page, like number of tours
-      if (skip >= numTours) throw new Error('This page does not exit!!'); 
-    }
-
     // EXECUTE QUERY
-    const tours = await query;
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     // query.sort().select().skip().limit() // query  chaining
 
-    // SEND RESPONSE  
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -78,12 +98,6 @@ exports.getAllTours = async (req, res) => {
     });
   }
 };
-
- // const tours = await Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');  // hard coded mongoose special methods
 
 exports.getTour = async (req, res) => {
   try {
