9b189a4f385c364b704cce2ae714261459117831
Parth Dhawan <pdhawan2001@gmail.com>
2021-10-28 21:25:01 +0530

    handled duplicate DB error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,6 +5,11 @@ const handleCastErrorDB = err => {
   return new AppError(message, 400);
 };
 
+const handleDuplicateFieldsDB =  err => { 
+  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -48,6 +53,10 @@ module.exports = (err, req, res, next) => {
     if (err.name === 'CastError') {
       error = handleCastErrorDB(error);
     }
+    if (err.code === 11000) { // for duplicate fields
+      error = handleDuplicateFieldsDB(error);
+    }  
+
     sendErrorProd(error, res);
   }
 };
