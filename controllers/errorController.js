4e8cf0f196942ba66c9c403420bbe812fab1dae2
Parth Dhawan <pdhawan2001@gmail.com>
2021-10-29 19:21:20 +0530

    handled validaton errors patch-1

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -11,7 +11,7 @@ const handleDuplicateFieldsDB =  err => {
 };
 
 const handleValidationErrorDB = err => {
-  const errors = Object.values(err.errors).map(el => el.message);
+  const errors = Object.values(err.errors).map(el => el.message); // ..map is used to lloop over the messages
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
