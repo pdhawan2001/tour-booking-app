e5f09f05d1fdc620d3ec71d609ef7087f09d0150
Parth Dhawan <pdhawan2001@gmail.com>
2021-11-09 16:33:43 +0530

    sending reset token through nodemailer

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+const sendEmail = require('../utils/email');
 
 // To create a new token
 const signToken = (
@@ -19,7 +20,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
-    role: req.body.role
+    role: req.body.role,
   }); // create user with this much data, because of security reasons not with full body, as anyone can specify his role as admin here
 
   const token = signToken(newUser._id);
@@ -88,38 +89,70 @@ exports.protect = catchAsync(async (req, res, next) => {
   }
 
   // 4) Check if user changed password after the token was issued.
-  if (currentUser.changedPasswordAfter(decoded.iat)) { // iat: issued at
-    return next(new AppError('User recently changed password! Please log in again', 401));
-  } 
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    // iat: issued at
+    return next(
+      new AppError('User recently changed password! Please log in again', 401)
+    );
+  }
 
   // GRANT ACCESS TO PROTECTED ROUTE
   req.user = currentUser; // put entire user data on the request, that is grant him access
   next();
 });
 
-exports.restrictTo = (...roles) => (req, res, next) => { // this function will get access to the roles parameter above
-     // roles is an array ['admin', 'lead-guide]. role='user' will not have permission
-    if(!roles.includes(req.user.role)) { // if the roles does not include lead-guide or admin
-      return next(new AppError('You do not have permission to perform this action', 403));
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    // this function will get access to the roles parameter above
+    // roles is an array ['admin', 'lead-guide]. role='user' will not have permission
+    if (!roles.includes(req.user.role)) {
+      // if the roles does not include lead-guide or admin
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
     }
 
     next();
   }; // a wrapper function which will return the middleware function
- 
-  exports.forgotPassword = catchAsync(async (req, res, next) => {
-    // 1) Get user based on posted email
-    const user = await User.findOne({ email: req.body.email }); /// because we don't know user's id and user also don't know his id
-    if(!user) {
-      return next(new AppError('There is no user with that email address.', 404));
-    }
 
-    // 2) Generate the random reset token
-    const resetToken = user.createPasswordResetToken(); 
-    await user.save({ validateBeforeSave: false }); // deactivate all validators that we sset in our schema
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on posted email
+  const user = await User.findOne({ email: req.body.email }); /// because we don't know user's id and user also don't know his id
+  if (!user) {
+    return next(new AppError('There is no user with that email address.', 404));
+  }
 
-    // 3) Send it back as an email
+  // 2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false }); // deactivate all validators that we set in our schema
+
+  // 3) Send it back as an email
+  // below basically the URL structure is recreated
+  const resetURL = `${req.protocol}://${req.get(
+    'host' 
+  )}/api/v1/users/resetPassword/${resetToken}`; // user will click on this email will be able to request from there to change password, req.get('host') so that it can handle both production and development
+
+  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email`;
+
+  try {
+    await sendEmail({
+      email: user.email, // or req.body.email
+      subject: 'Your password reset token (valid for 10 mins)',
+      message
+    });
+  } catch(err) { // because if there is an error we want to reset the token and expires property, and just not send the error itself
+    user.passwordResetToken = undefined;
+    user.passwordResetExpires = undefined;
+    await user.save({ validateBeforeSave: false }); // this is because above property just modifies the data and does not saves it
+
+    return next(new AppError('There was an error sending the email. Try again later!', 500));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Token sent to email!'
   });
+});
 
-  exports.resetPassword = (req, res, next) => {
-
-  };
+exports.resetPassword = (req, res, next) => {};
---
+++ utils/email.js
@@ -0,0 +1,28 @@
+const nodemailer = require('nodemailer');
+
+const sendEmail = async options => {
+    // 1) Create a transporter
+    const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+        }
+        // Activate in gmail "less secure app" option
+    });
+
+    // 2) Define the email options
+    const mailOptions = {
+        from: 'Parth Dhawan <pdhawan2001@gmail.com>',
+        to: options.email, // options opject is what we passed above to the function,
+        subject: options.subject,
+        text: options.message,
+        // html: .
+    }
+
+    // 3) Actually send the email
+    await transporter.sendMail(mailOptions)
+};
+
+module.exports = sendEmail;
