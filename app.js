1eca20b6ba305c723280ba833c06444f852d7406
Parth Dhawan <pdhawan2001@gmail.com>
2021-11-11 15:08:11 +0530

    implemented helmet to secure HTTP headers

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,26 +11,29 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1)GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet()); 
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+// Limit requests from same API
 const limiter = rateLimit({
   max: 100, // maxx number of requests allowed
   windowMs: 60 * 60 * 1000, // 1 hour window, that is if crossed 100 request it will prompt an error message and then user have to wait for 1 hour
-  message: 'Too many requests from this IP, please try again in an hour!'
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
-
 app.use('/api', limiter); // apply to route that start with /api
 
-app.use(express.json()); //middleware
-app.use(express.static(`${__dirname}/public`)); // to serve static file
+// Body parser, reading data from body req.body
+app.use(express.json({ limit: '10kb' })); // it will limit data of body to take only 10kb
 
-// app.use((req, res, next) => {
-//   console.log('Hello from middleware 👋🏻️');
-//   next();
-// });
+// Serving static file
+app.use(express.static(`${__dirname}/public`));
 
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); // convert date and time to readable string
   // console.log(req.headers);
