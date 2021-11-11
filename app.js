4b20636275c355d4818b794ad9eab014fd811fc1
Parth Dhawan <pdhawan2001@gmail.com>
2021-11-11 14:52:27 +0530

    implemented rate limiting

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,12 +9,21 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARES 
-if(process.env.NODE_ENV === 'development') {
+// 1)GLOBAL MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+  max: 100, // maxx number of requests allowed
+  windowMs: 60 * 60 * 1000, // 1 hour window, that is if crossed 100 request it will prompt an error message and then user have to wait for 1 hour
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+
+app.use('/api', limiter); // apply to route that start with /api
+
 app.use(express.json()); //middleware
-app.use(express.static(`${__dirname}/public`)); // to serve static file 
+app.use(express.static(`${__dirname}/public`)); // to serve static file
 
 // app.use((req, res, next) => {
 //   console.log('Hello from middleware 👋🏻️');
@@ -26,12 +36,12 @@ app.use((req, res, next) => {
   next();
 });
 
-// 2) ROUTES 
+// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 // if any request reached this point of code that means it wasn't handled by any of the above router functions, because middleware executes line by line
-app.all('*', (req, res, next) => {   
+app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)); // it will skip all other middleware and go directly to error handling middleware
 }); // all and star for every route
 
