089c47acc9b58c534db59901eaace749c1dded0b
Parth Dhawan <pdhawan2001@gmail.com>
2021-12-06 11:04:25 +0530

    Added Password Change Page

--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -3,21 +3,22 @@
 import axios from 'axios';
 import { showAlert } from './alert';
 
-export const updateData = async (name, email) => {
+// type is either password or data
+export const updateSettings = async (data, type) => {
   try {
+    const url =
+      type === 'password'
+        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
+        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
     const res = await axios({
       method: 'PATCH',
-      url: 'http://127.0.0.1:3000/api/v1/users/updateMe',
-      data: {
-        name,
-        email,
-      },
+      url,
+      data,
     }); // axios can also send an error from the API
 
     if (res.data.status === 'success') {
-      showAlert('success', 'Data updated successfully!');
+      showAlert('success', `${type.toUpperCase()} updated successfully!`);
     }
-
   } catch (err) {
     showAlert('error', err.response.data.message); // this comes from axios documentation
   }
